199513e8457f78d153d204c86462f6a708c58277
itsme-didulabot <ddidula034@gmail.com>
2025-02-07 19:21:56 +0530

    Update index.js

--- index.js
+++ index.js
@@ -74,22 +74,21 @@ conn.sendMessage(ownerNumber + "@s.whatsapp.net", { image: { url: `https://teleg
 conn.ev.on('creds.update', saveCreds)  
 
 conn.ev.on('messages.upsert', async(mek) => {
-mek = mek.messages[0]
-if (!mek.message) return	
-mek.message = (getContentType(mek.message) === 'ephemeralMessage') ? mek.message.ephemeralMessage.message : mek.message
-if (mek.key && mek.key.remoteJid === 'status@broadcast'){
-      await conn.readMessages([mek.key])
+    mek = mek.messages[0]
+    if (!mek.message) return        
+    mek.message = (getContentType(mek.message) === 'ephemeralMessage') ? mek.message.ephemeralMessage.message : mek.message
+    if (mek.key && mek.key.remoteJid === 'status@broadcast') {
+      let emoji = [
+        '😘', '😭', '😂', '😹', '😍', '😋', '🙏', '😜', '😢', '😠', '🤫', '😎',
+      ];
+      let sigma = emoji[Math.floor(Math.random() * emoji.length)];
+      await conn.readMessages([mek.key]);
+      conn.sendMessage(
+        'status@broadcast',
+        { react: { text: sigma, key: mek.key } },
+        { statusJidList: [mek.key.participant] },
+      );
     }
-  if (mek.key && mek.key.remoteJid === 'status@broadcast'){
-    const emojis = ['🧩', '🍉', '💜', '🌸', '🪴', '💊', '💫', '🍂', '🌟', '🎋', '😶‍🌫️', '🫀', '🧿', '👀', '🤖', '🚩', '🥰', '🗿', '💜', '💙', '🌝', '🖤', '💚'];
-    const randomEmoji = emojis[Math.floor(Math.random() * emojis.length)];
-    await conn.sendMessage(mek.key.remoteJid, {
-      react: {
-        text: randomEmoji,
-        key: mek.key,
-      } 
-    }, { statusJidList: [mek.key.participant] });
-  }
 const m = sms(conn, mek)
 const type = getContentType(mek.message)
 const content = JSON.stringify(mek.message)
