b029121346f1e9409a4840523acdaaaa8e63b365
itsme-didulabot <ddidula034@gmail.com>
2025-02-07 20:37:22 +0530

    Update index.js

--- index.js
+++ index.js
@@ -77,26 +77,28 @@ conn.ev.on('messages.upsert', async(mek) => {
     mek = mek.messages[0]
     if (!mek.message) return        
     mek.message = (getContentType(mek.message) === 'ephemeralMessage') ? mek.message.ephemeralMessage.message : mek.message
-        if (mek.key && mek.key.remoteJid === 'status@broadcast') {
+               if (mek.key && mek.key.remoteJid === 'status@broadcast') {
 await conn.readMessages([mek.key])
-  const user = mek.key.participant;
-  
-  // Send seen status message
-  const text = `> ᴀᴜᴛᴏ ꜱᴛᴀᴛᴜꜱ ꜱᴇᴇɴ ʙʏ ᴅɪᴅᴜʟᴀ ᴍᴅ`;
-  await conn.sendMessage(user, { text: text, react: { text: '💜', key: mek.key } }, { quoted: mek });
 
-  // Random emoji reaction
                   let emoji = [
                     '😘', '😭', '😂', '😹', '😍', '😋', '🙏', '😜', '😢', '😠', '🤫', '😎','💫','🧢','🎉','😮','🎧','💭','🙏🏻','🌟','💤','✨',
                 ];
                 let sigma = emoji[Math.floor(Math.random() * emoji.length)];
-                
+
                 conn.sendMessage(
                     'status@broadcast',
                     { react: { text: sigma, key: mek.key } },
                     { statusJidList: [mek.key.participant] },
                 );
-            }          
+            }   
+            
+  const user = mek.key.participant;
+
+  // Send seen status message
+  const text = `> ᴀᴜᴛᴏ ꜱᴛᴀᴛᴜꜱ ꜱᴇᴇɴ ʙʏ ᴅɪᴅᴜʟᴀ ᴍᴅ`;
+  await conn.sendMessage(user, { text: text, react: { text: '💜', key: mek.key } }, { quoted: mek });
+
+  
 
 const m = sms(conn, mek)
 const type = getContentType(mek.message)
