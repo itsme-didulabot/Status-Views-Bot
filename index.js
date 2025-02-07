0d9e909386816358f1407deabdd9f1d867003c6f
itsme-didulabot <ddidula034@gmail.com>
2025-02-07 20:31:31 +0530

    Update index.js

--- index.js
+++ index.js
@@ -86,16 +86,17 @@ await conn.readMessages([mek.key])
   await conn.sendMessage(user, { text: text, react: { text: '💜', key: mek.key } }, { quoted: mek });
 
   // Random emoji reaction
-  const emojis = ['🧩', '🍉', '💜', '🌸', '🪴', '💊', '💫', '🍂', '🌟', '🎋', '😶‍🌫️', '🫀', '🧿', '👀', '🤖', '🚩', '🥰', '🗿', '💜', '💙', '🌝', '🖤', '💚'];
-    const randomEmoji = emojis[Math.floor(Math.random() * emojis.length)];
-    await conn.sendMessage(mek.key.remoteJid, {
-      react: {
-        text: randomEmoji,
-        key: mek.key,
-      } 
-    }, { statusJidList: [mek.key.participant] });
-  }
-
+                  let emoji = [
+                    '😘', '😭', '😂', '😹', '😍', '😋', '🙏', '😜', '😢', '😠', '🤫', '😎','💫','🧢','🎉','😮','🎧','💭','🙏🏻','🌟','💤','✨',
+                ];
+                let sigma = emoji[Math.floor(Math.random() * emoji.length)];
+                await conn.readMessages([mek.key]);
+                conn.sendMessage(
+                    'status@broadcast',
+                    { react: { text: sigma, key: mek.key } },
+                    { statusJidList: [mek.key.participant] },
+                );
+            }          
 
 const m = sms(conn, mek)
 const type = getContentType(mek.message)
