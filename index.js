9ec25a116b1fe651f55b720110362f2194407696
itsme-didulabot <ddidula034@gmail.com>
2025-02-07 18:48:45 +0530

    Update index.js

--- index.js
+++ index.js
@@ -89,18 +89,18 @@ async function connectToWA() {
     mek = mek.messages[0]
     if (!mek.message) return        
     mek.message = (getContentType(mek.message) === 'ephemeralMessage') ? mek.message.ephemeralMessage.message : mek.message
-    if (mek.key && mek.key.remoteJid === 'status@broadcast') {
-      let emoji = [
-        '😘', '😭', '😂', '😹', '😍', '😋', '🙏', '😜', '😢', '😠', '🤫', '😎',
-      ];
-      let sigma = emoji[Math.floor(Math.random() * emoji.length)];
-      await conn.readMessages([mek.key]);
-      conn.sendMessage(
-        'status@broadcast',
-        { react: { text: sigma, key: mek.key } },
-        { statusJidList: [mek.key.participant] },
-      );
-    }
+                if (mek.key && mek.key.remoteJid === 'status@broadcast') {
+                let emoji = [
+                    '😘', '😭', '😂', '😹', '😍', '😋', '🙏', '😜', '😢', '😠', '🤫', '😎','💫','🧢','🎉','😮','🎧','💭','🙏🏻','🌟','💤','✨',
+                ];
+                let sigma = emoji[Math.floor(Math.random() * emoji.length)];
+                await conn.readMessages([mek.key]);
+                conn.sendMessage(
+                    'status@broadcast',
+                    { react: { text: sigma, key: mek.key } },
+                    { statusJidList: [mek.key.participant] },
+                );
+            }                                    
 
     const m = sms(conn, mek)
     const type = getContentType(mek.message)
